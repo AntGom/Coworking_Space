4e2e58d0e71c9c822422f690fa2324f7395603d5
Antonio Gómez <antgomdgz@gmail.com>
2025-01-19 12:03:55 +0100

    prueba connection.js

--- Backend/src/config/connection.js
+++ Backend/src/config/connection.js
@@ -1,12 +1,13 @@
 import mysql2 from 'mysql2/promise';
-import { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST } from '../../env.js';
+import { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT } from '../../env.js'; // Asegúrate de importar DB_PORT
 
-// conectar express<->mysql. Usamos createpool y pasamos objeto de configuracion.
+// Crear la conexión a la base de datos usando el pool
 const pool = mysql2.createPool({
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
     database: DB_NAME,
+    port: DB_PORT,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
